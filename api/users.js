6d70635e5ca1f5f39fec44d0e17dd51ce12a6a3f
Zakhar <zahasenok@gmail.com>
2020-07-03 00:16:20 +0300

    Users' CRUD implementation

--- api/users.js
+++ api/users.js
@@ -49,9 +49,68 @@ router.get('/authorization', async(req, res) => {
         res.status(response_data.status).json({ error: response_data.body });
     }
 });
-router.get('/check', (req, res) => {
+router.get('/info', async(req, res) => {
+    const user = await getUserByToken(req.query.token);
+    console.log(user);
+    if (user) {
+        if (user.expire_date >= Date.now()) res.status(200).json([{ Status: `Logged in as ${user.first_name} ${user.last_name}` }, { first_name: user.first_name, last_name: user.last_name, email: user.email }]);
+        else res.status(400).json({ error: "Invalid token, please relog" });
+    } else {
+        res.status(400).json({ error: "Invalid token, please relog" });
+    }
+});
+router.put('/update', urlencodedParser, async(req, res) => {
+    const user = await getUserByToken(req.query.token);
+    const changes = [];
+    const errors = {};
+    if (user && req.body) {
+        if (req.body.first_name) {
+            user.first_name = req.body.first_name;
+            changes.push("First Name");
+        }
+        if (req.body.last_name) {
+            user.last_name = req.body.last_name;
+            changes.push("Last Name");
+        }
+        if (req.body.email) {
+            if (await isEmailUnique(req.body.email, user.user_id)) {
+                user.email = req.body.email;
+                changes.push("Email");
 
-})
+            } else errors.update_email_error = "User with this email had already been registered";
+        }
+        if (req.body.new_password) {
+            if (crypto.SHA512(req.body.old_password) == user.password) {
+                user.password = crypto.SHA512(req.body.new_password);
+                changes.push("Password");
+            } else {
+                errors.update_password_error = "Old password is wrong";
+            }
+        }
+        if (errors.update_email_error || errors.update_password_error) res.status(400).json(errors);
+        else {
+            if (changes.length == 0) res.status(200).json({ error: "Body is missing. Nothing to update" });
+            else {
+                const response_data = await updateUserById(user, user.user_id);
+
+                res.status(response_data.status).json({ updated_data: changes });
+            }
+        }
+    } else {
+        res.status(400).json({ error: "Wrong Token" });
+    }
+});
+router.delete('/delete', urlencodedParser, async(req, res) => {
+    const user = await getUserByToken(req.query.token);
+    if (req.body && user) {
+        if (crypto.SHA512(req.body.password) == user.password) {
+            const response_data = await removeUserById(user.user_id);
+            res.status(response_data.status).json(response_data.body);
+        }
+    } else {
+        res.status(400).json({ error: "Wrong password or token" });
+    }
+});
 async function regDataValid(reg_data) {
     if (!reg_data.first_name || !reg_data.last_name || !reg_data.email || !reg_data.password) return { isErr: true, body: "Empty fields exist", code: 400 };
     if (!await isEmailUnique(reg_data.email)) return { isErr: true, body: "User with such email is already exists", code: 400 };
@@ -60,6 +119,20 @@ async function regDataValid(reg_data) {
 
 async function isEmailUnique(email) {
     const sql = `SELECT * FROM users WHERE email="${email}"`;
+    return connection.promise().query(sql)
+        .then(result => {
+            if (result[0][0]) {
+                return false;
+            } else {
+                return true;
+            }
+        })
+        .catch(err => {
+            console.log(`Error while checking email: ${ err }`);
+        });
+}
+async function isEmailUnique(email, id) {
+    const sql = `SELECT * FROM users WHERE email="${email}" AND user_id <> ${id}`;
     return connection.promise().query(sql)
         .then(result => {
             if (result[0][0]) {
@@ -103,6 +176,39 @@ async function generateToken(user) {
         })
         .catch(err => console.log(err));
 }
-
+async function getUserByToken(token) {
+    const sql = `SELECT * FROM users WHERE token="${token}"`;
+    return connection.promise().query(sql)
+        .then(result => {
+            if (result[0][0]) {
+                return result[0][0];
+            } else {
+                return null;
+            }
+        })
+        .catch(err => {
+            console.log(`Error while getting user: ${ err }`);
+        });
+}
+async function updateUserById(user, id) {
+    const sql = `UPDATE users SET first_name='${user.first_name}', last_name='${user.last_name}',email='${user.email}',password='${user.password}' WHERE user_id =${user.user_id}`;
+    return connection.promise().query(sql)
+        .then(result => {
+            return { status: 200, user: user }
+        })
+        .catch(err => {
+            return { status: 500, user: null }
+        });
+}
+async function removeUserById(id) {
+    const sql = `DELETE FROM users WHERE user_id=${id}`;
+    return connection.promise().query(sql)
+        .then(result => {
+            return { status: 200, body: { success: "Successfully deleted" } }
+        })
+        .catch(err => {
+            return { status: 500, body: { error: err } };
+        })
+}
 
 module.exports = router;
